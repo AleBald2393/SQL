d3e9b07845cdf2b0fc9783f35eeee50777ce80c8
AleBald2393 <166253963+AleBald2393@users.noreply.github.com>
2024-06-10 19:29:41 -0500

    Update Connections.py

--- Teoría/Connections.py
+++ Teoría/Connections.py
@@ -27,3 +27,58 @@ cursor_obj.execute('''insert into INSTRUCTOR values (2, 'Raul', 'Chong', 'Markha
 
 Query data in the table¶
 In this step we will retrieve data we inserted into the INSTRUCTOR table.
+
+ statement = '''SELECT * FROM INSTRUCTOR'''
+cursor_obj.execute(statement)
+
+print("All the data")
+output_all = cursor_obj.fetchall()
+for row_all in output_all:
+  print(row_all)
+
+## Fetch few rows from the table
+statement = '''SELECT * FROM INSTRUCTOR'''
+cursor_obj.execute(statement)
+  
+print("All the data")
+# If you want to fetch few rows from the table we use fetchmany(numberofrows) and mention the number how many rows you want to fetch
+output_many = cursor_obj.fetchmany(2) 
+for row_many in output_many:
+  print(row_many)
+
+# Fetch only FNAME from the table
+statement = '''SELECT FNAME FROM INSTRUCTOR'''
+cursor_obj.execute(statement)
+  
+print("All the data")
+output_column = cursor_obj.fetchall()
+for fetch in output_column:
+  print(fetch)
+
+now write and execute an update statement that changes the Rav's CITY to MOOSETOWN
+query_update='''update INSTRUCTOR set CITY='MOOSETOWN' where FNAME="Rav"'''
+cursor_obj.execute(query_update)
+
+statement = '''SELECT * FROM INSTRUCTOR'''
+cursor_obj.execute(statement)
+  
+print("All the data")
+output1 = cursor_obj.fetchmany(2)
+for row in output1:
+  print(row)
+
+Retrieve data into Pandas
+import pandas as pd
+#retrieve the query results into a pandas dataframe
+df = pd.read_sql_query("select * from instructor;", conn)
+
+#print the dataframe
+df
+
+#print just the LNAME for first row in the pandas data frame
+df.LNAME[0]
+
+df.shape
+
+# Close the connection
+conn.close()
