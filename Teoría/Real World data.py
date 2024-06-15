5412432f5ee856ac4557958c8f401da42738fac6
AleBald2393 <166253963+AleBald2393@users.noreply.github.com>
2024-06-15 11:55:33 -0500

    Update Real World data.py

--- Teoría/Real World data.py
+++ Teoría/Real World data.py
@@ -32,3 +32,11 @@ df.to_sql("CHICAGO_PUBLIC_SCHOOLS_DATA", con, if_exists='replace', index=False, 
 #cual es el colegio con el dato mayor
 %sql select Name_of_School, Safety_Score from CHICAGO_PUBLIC_SCHOOLS_DATA where \
   Safety_Score= (select MAX(Safety_Score) from CHICAGO_PUBLIC_SCHOOLS_DATA)
+
+#cual es el top 10 con el mayor promedio 
+%sql select Name_of_School, Average_Student_Attendance from CHICAGO_PUBLIC_SCHOOLS_DATA \
+    order by Average_Student_Attendance desc nulls last limit 10 
+
+#cual es el top 5 de las escuelas que no fueron
+%sql select Name_of_School, Average_Student_Attendance from CHICAGO_PUBLIC_SCHOOLS_DATA \
+    order by Average_Student_Attendance limit 5 
