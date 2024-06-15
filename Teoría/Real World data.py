77cf3548b8ef947f60e02053ff2f38f080bfba71
AleBald2393 <166253963+AleBald2393@users.noreply.github.com>
2024-06-15 11:31:20 -0500

    Update Real World data.py

--- Teoría/Real World data.py
+++ Teoría/Real World data.py
@@ -22,3 +22,6 @@ df.to_sql("CHICAGO_PUBLIC_SCHOOLS_DATA", con, if_exists='replace', index=False, 
 
 # type in your query to retrieve all column names in the SCHOOLS table along with their datatypes and length
 %sql SELECT name,type,length(type) FROM PRAGMA_TABLE_INFO('CHICAGO_PUBLIC_SCHOOLS_DATA');
+
+#Cuantas Elementary schools hay en la data
+%sql SELECT COUNT(*) FROM CHICAGO_PUBLIC_SCHOOLS_DATA Where "Elementary, Middle, or High School"='ES'
