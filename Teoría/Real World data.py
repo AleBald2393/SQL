ade4132e106a39d91435cb6c42640b1b9a6ed91e
AleBald2393 <166253963+AleBald2393@users.noreply.github.com>
2024-06-15 11:12:27 -0500

    Update Real World data.py

--- Teoría/Real World data.py
+++ Teoría/Real World data.py
@@ -8,3 +8,8 @@ cur = con.cursor()
 %load_ext sql
 
 %sql sqlite:///RealWorldData.db
+
+import pandas
+
+df = pandas.read_csv("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DB0201EN-SkillsNetwork/labs/FinalModule_Coursera_V5/data/ChicagoPublicSchools.csv")
+df.to_sql("CHICAGO_PUBLIC_SCHOOLS_DATA", con, if_exists='replace', index=False, method="multi")
